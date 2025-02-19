1acf5db7b1c79fbc13d064762a9aab2ecfda2121
s00been <s00been1313@gmail.com>
2025-02-20 00:16:05 +0900

    상품 등록 기능 구현현

--- react-project/ts-shop/src/pages/Products.tsx
+++ react-project/ts-shop/src/pages/Products.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Grid, Container, Paper, useTheme } from '@mui/material';
+import {
+  Grid,
+  Container,
+  Paper,
+  useTheme,
+  Fab,
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 
 import ProductCard from '../components/Product/ProductCard';
 import { cartItemsVar } from '../cache';
@@ -14,7 +28,20 @@ export default function Products() {
   const theme = useTheme();
 
   const { data, loading, error } = useGetProductsQuery();
-  const [addProduct, { loading: mutationLoading }] = useAddProductMutation();
+  const [addProduct, { loading: mutationLoading }] = useAddProductMutation({
+    onCompleted: (data) => {
+      console.log(data);
+    },
+    refetchQueries: ['GetProducts'],
+  });
+
+  const [open, setOpen] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    price: '',
+    imgUrl: '',
+  });
+  const { name, price, imgUrl } = form;
 
   if (loading) {
     return (
@@ -33,6 +60,31 @@ export default function Products() {
       </Container>
     );
   }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleTextChange = (e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = () => {
+    addProduct({
+      variables: form,
+    })
+      .then((res) => {
+        console.log(res);
+        handleClose();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <Container sx={{ paddingTop: 7 }}>
@@ -56,6 +108,67 @@ export default function Products() {
           </Grid>
         ))}
       </Grid>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          paper: {
+            component: 'form',
+            onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+              const formData = new FormData(event.currentTarget);
+              const formJson = Object.fromEntries((formData as any).entries());
+              const email = formJson.email;
+              console.log(email);
+              handleClose();
+            },
+          },
+        }}
+      >
+        <DialogTitle>상품 등록</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <code>{JSON.stringify(form)}</code>
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            name="name"
+            label="상품명"
+            fullWidth
+            value={name}
+            onChange={handleTextChange}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            name="price"
+            label="상품 가격"
+            fullWidth
+            value={price}
+            onChange={handleTextChange}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            name="imgUrl"
+            label="상품 이미지 주소"
+            fullWidth
+            value={imgUrl}
+            onChange={handleTextChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Add</Button>
+        </DialogActions>
+      </Dialog>
+      <Fab sx={{ position: 'fixed', bottom: 16, right: 16 }}>
+        <AddTwoToneIcon onClick={handleClickOpen} />
+      </Fab>
     </Container>
   );
 }
